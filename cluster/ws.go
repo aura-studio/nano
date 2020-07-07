0f19f9723347c3af27a26c7da7658f12d01b4af2
lizongti <lzongti@gmail.com>
2020-07-07 19:50:38 +0800

    fix ws bug

--- cluster/ws.go
+++ cluster/ws.go
@@ -28,25 +28,17 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// wsConn is an adapter to t.Conn, which implements all t.Conn
+// WSConn is an adapter to t.Conn, which implements all t.Conn
 // interface base on *websocket.Conn
-type wsConn struct {
+type WSConn struct {
 	conn   *websocket.Conn
 	typ    int // message type
 	reader io.Reader
 }
 
-// NewWSConn return an initialized *wsConn
-func NewWSConn(conn *websocket.Conn) (*wsConn, error) {
-	c := &wsConn{conn: conn}
-
-	t, r, err := conn.NextReader()
-	if err != nil {
-		return nil, err
-	}
-
-	c.typ = t
-	c.reader = r
+// NewWSConn return an initialized *WSConn
+func NewWSConn(conn *websocket.Conn) (*WSConn, error) {
+	c := &WSConn{conn: conn}
 
 	return c, nil
 }
@@ -54,7 +46,16 @@ func NewWSConn(conn *websocket.Conn) (*wsConn, error) {
 // Read reads data from the connection.
 // Read can be made to time out and return an Error with Timeout() == true
 // after a fixed time limit; see SetDeadline and SetReadDeadline.
-func (c *wsConn) Read(b []byte) (int, error) {
+func (c *WSConn) Read(b []byte) (int, error) {
+	if c.reader == nil {
+		t, r, err := c.conn.NextReader()
+		if err != nil {
+			return 0, err
+		}
+		c.typ = t
+		c.reader = r
+	}
+
 	n, err := c.reader.Read(b)
 	if err != nil && err != io.EOF {
 		return n, err
@@ -72,7 +73,7 @@ func (c *wsConn) Read(b []byte) (int, error) {
 // Write writes data to the connection.
 // Write can be made to time out and return an Error with Timeout() == true
 // after a fixed time limit; see SetDeadline and SetWriteDeadline.
-func (c *wsConn) Write(b []byte) (int, error) {
+func (c *WSConn) Write(b []byte) (int, error) {
 	err := c.conn.WriteMessage(websocket.BinaryMessage, b)
 	if err != nil {
 		return 0, err
@@ -83,17 +84,17 @@ func (c *wsConn) Write(b []byte) (int, error) {
 
 // Close closes the connection.
 // Any blocked Read or Write operations will be unblocked and return errors.
-func (c *wsConn) Close() error {
+func (c *WSConn) Close() error {
 	return c.conn.Close()
 }
 
 // LocalAddr returns the local network address.
-func (c *wsConn) LocalAddr() net.Addr {
+func (c *WSConn) LocalAddr() net.Addr {
 	return c.conn.LocalAddr()
 }
 
 // RemoteAddr returns the remote network address.
-func (c *wsConn) RemoteAddr() net.Addr {
+func (c *WSConn) RemoteAddr() net.Addr {
 	return c.conn.RemoteAddr()
 }
 
@@ -112,7 +113,7 @@ func (c *wsConn) RemoteAddr() net.Addr {
 // the deadline after successful Read or Write calls.
 //
 // A zero value for t means I/O operations will not time out.
-func (c *wsConn) SetDeadline(t time.Time) error {
+func (c *WSConn) SetDeadline(t time.Time) error {
 	if err := c.conn.SetReadDeadline(t); err != nil {
 		return err
 	}
@@ -123,7 +124,7 @@ func (c *wsConn) SetDeadline(t time.Time) error {
 // SetReadDeadline sets the deadline for future Read calls
 // and any currently-blocked Read call.
 // A zero value for t means Read will not time out.
-func (c *wsConn) SetReadDeadline(t time.Time) error {
+func (c *WSConn) SetReadDeadline(t time.Time) error {
 	return c.conn.SetReadDeadline(t)
 }
 
@@ -132,6 +133,6 @@ func (c *wsConn) SetReadDeadline(t time.Time) error {
 // Even if write times out, it may return n > 0, indicating that
 // some of the data was successfully written.
 // A zero value for t means Write will not time out.
-func (c *wsConn) SetWriteDeadline(t time.Time) error {
+func (c *WSConn) SetWriteDeadline(t time.Time) error {
 	return c.conn.SetWriteDeadline(t)
 }
