164663a30603314c9a675b33021f8df5531a42c9
lizongti <lzongti@gmail.com>
2020-08-28 17:37:13 +0800

    adjust convention

--- cluster/convention.go
+++ cluster/convention.go
@@ -14,7 +14,7 @@ type (
 
 	// Convention establish a connection
 	Convention interface {
-		Establish(Transmitter, Acceptor)
+		Establish(Transmitter) Acceptor
 	}
 
 	// transmitter is to implement Transmitter
@@ -29,12 +29,12 @@ type (
 	}
 )
 
+// NewConventioner creates a new conventioner
 func NewConventioner(convention Convention) Conventioner {
 	var transmitter = &transmitter{
 		node: CurrentNode,
 	}
-	var acceptor Acceptor
-	convention.Establish(transmitter, acceptor)
+	acceptor := convention.Establish(transmitter)
 
 	return Conventioner{
 		Transmitter: transmitter,
@@ -42,10 +42,12 @@ func NewConventioner(convention Convention) Conventioner {
 	}
 }
 
+// Unicast implements Transmitter.Unicast
 func (t *transmitter) Unicast(label string, sig int64, msg []byte) ([]byte, error) {
 	return t.node.cluster.Unicast(label, sig, msg)
 }
 
+// Unicast implements Transmitter.Multicast
 func (t *transmitter) Multicast(sig int64, msg []byte) ([][]byte, error) {
 	return t.node.cluster.Multicast(sig, msg)
 }
