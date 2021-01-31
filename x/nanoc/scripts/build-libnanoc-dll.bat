27f0a9386d1efb379b704aad959c8cffbfb2b051
lizongti <lzongti@gmail.com>
2021-01-31 13:19:20 +0800

    move x out

--- x/nanoc/scripts/build-libnanoc-dll.bat
+++
@@ -1,4 +0,0 @@
-
-if not exist "nanoc" (cd ..)
-if not exist "nanoc" (cd ..)
-go build -o ./bin/libnanoc.dll -buildmode=c-shared ./nanoc/
--- x/nanoc/scripts/build-libnanoc-so.bat
+++
@@ -1,5 +0,0 @@
-if not exist "nanoc" (cd ..)
-if not exist "nanoc" (cd ..)
-docker run -it --rm ^
--v %cd%:/go/src/nanox ^
-golang:latest go env -w GOPROXY=https://goproxy.cn,direct && go build -o ./bin/libnanoc.so -buildmode=c-shared ./nanoc/
