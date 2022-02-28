6e6399e8d065a436d6db61933e0a2c8d81ad2c98
lizongti <lzongti@gmail.com>
2022-02-28 19:00:31 +0800

    fix state

--- state/state.go
+++ state/state.go
@@ -22,6 +22,8 @@ type State struct {
 
 	mods []StateMod
 	mu   sync.RWMutex
+
+	facade *Facade
 }
 
 func NewState() *State {
@@ -29,6 +31,10 @@ func NewState() *State {
 		data: sync.Map{},
 		mods: make([]StateMod, 0),
 	}
+}
+
+func (s *State) Facade() *Facade {
+	return s.facade
 }
 
 func (s *State) Set(k string, v interface{}) {
@@ -40,6 +46,8 @@ func (s *State) Set(k string, v interface{}) {
 		Action: Set,
 		Value:  cast.ToString(v),
 	})
+
+	s.facade.set(k, v)
 }
 
 func (s *State) Append(k string, v interface{}) {
@@ -51,6 +59,8 @@ func (s *State) Append(k string, v interface{}) {
 		Action: Append,
 		Value:  cast.ToString(v),
 	})
+
+	s.facade.append(k, v)
 }
 
 func (s *State) Dump() []StateMod {
@@ -76,6 +86,7 @@ func (s *State) Update(data map[string]string) {
 	for k, v := range data {
 		s.data.Store(k, v)
 	}
+	s.facade.update(data)
 }
 
 func (s *State) Int(key string) int {
@@ -166,4 +177,103 @@ func (c *Center) Data() map[string]string {
 	}
 
 	return data
+}
+
+type Facade struct {
+	data  sync.Map
+	state *State
+}
+
+func NewFacade(state *State) *Facade {
+	return &Facade{
+		data:  sync.Map{},
+		state: state,
+	}
+}
+
+func (f *Facade) Facade() *Facade {
+	return f
+}
+
+func (f *Facade) Set(k string, v interface{}) {
+	f.state.Facade()
+}
+
+func (f *Facade) set(k string, v interface{}) {
+	f.data.Store(k, v)
+}
+
+func (f *Facade) Append(k string, v interface{}) {
+	f.state.Append(k, v)
+}
+
+func (f *Facade) append(k string, v interface{}) {
+	if _, ok := f.data.Load(k); !ok {
+		f.data.Store(k, "0")
+	}
+	vOrigin, _ := f.data.Load(k)
+	f.data.Store(k, cast.ToString(cast.ToFloat64(vOrigin)+cast.ToFloat64(v)))
+}
+
+func (f *Facade) Dump() []StateMod {
+	return f.state.Dump()
+}
+
+func (f *Facade) Data() map[string]string {
+	data := make(map[string]string)
+	f.data.Range(func(key, value interface{}) bool {
+		data[cast.ToString(key)] = cast.ToString(value)
+		return true
+	})
+	return data
+}
+
+func (f *Facade) Update(data map[string]string) {
+	f.state.Update(data)
+}
+
+func (f *Facade) update(data map[string]string) {
+	for k, v := range data {
+		f.data.Store(k, v)
+	}
+}
+
+func (f *Facade) Int(key string) int {
+	v, ok := f.data.Load(key)
+	if !ok {
+		return 0
+	}
+	return cast.ToInt(v)
+}
+
+func (f *Facade) Int64(key string) int64 {
+	v, ok := f.data.Load(key)
+	if !ok {
+		return 0
+	}
+	return cast.ToInt64(v)
+}
+
+func (f *Facade) Float64(key string) float64 {
+	v, ok := f.data.Load(key)
+	if !ok {
+		return 0
+	}
+	return cast.ToFloat64(v)
+}
+
+func (f *Facade) String(key string) string {
+	v, ok := f.data.Load(key)
+	if !ok {
+		return ""
+	}
+	return cast.ToString(v)
+}
+
+func (f *Facade) Bool(key string) bool {
+	v, ok := f.data.Load(key)
+	if !ok {
+		return false
+	}
+	return cast.ToBool(v)
 }
