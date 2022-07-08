da7aaa4e30a45dd82159573307e691ef7e52032a
lizongti <lzongti@gmail.com>
2022-07-08 17:31:23 +0800

    remove log

--- cluster/handler.go
+++ cluster/handler.go
@@ -315,7 +315,6 @@ func (h *LocalHandler) handle(conn net.Conn) {
 		}
 
 		if agent.payloadLength < 1024 {
-			log.Debugf("Payload %d bytes from [%s]: %v", n, conn.RemoteAddr().String(), buf[:n])
 			agent.payloadLength += n
 		}
 
