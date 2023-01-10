8ecfde3cf1c02674266f7000d2a44f6d4f219635
lizongti <lzongti@gmail.com>
2023-01-10 18:42:59 +0800

    fix syscall_windows.go

--- scheduler/syscall_windows.go
+++ scheduler/syscall_windows.go
@@ -8,7 +8,8 @@ import "syscall"
 func init() {
 	winmmDLL := syscall.NewLazyDLL("winmm.dll")
 	procTimeBeginPeriod := winmmDLL.NewProc("timeBeginPeriod")
-	if _, _, err := procTimeBeginPeriod.Call(uintptr(1)); err != nil {
+	_, _, err := procTimeBeginPeriod.Call(uintptr(1))
+	if err != nil && err.Error() != "The operation completed successfully." {
 		panic(err)
 	}
 }
