d18738b5b99897021a356caef3c26ab70ee39b1e
lizongti <lzongti@gmail.com>
2023-07-19 21:01:02 +0800

    fix RPC

--- cluster/agent.go
+++ cluster/agent.go
@@ -141,9 +141,9 @@ func (a *agent) Push(route string, v interface{}) error {
 }
 
 func (a *agent) RPC(mid uint64, route string, v interface{}) error {
-	if a.status() == statusClosed {
-		return ErrBrokenPipe
-	}
+	// if a.status() == statusClosed {
+	// 	return ErrBrokenPipe
+	// }
 
 	if env.Debug {
 		switch d := v.(type) {
