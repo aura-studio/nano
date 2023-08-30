3f1652f29247b309ea43caf0c954230934e61854
lizongti <lzongti@gmail.com>
2023-08-30 18:25:11 +0800

    fix copyData

--- codec/legacycodec/legacycodec.go
+++ codec/legacycodec/legacycodec.go
@@ -63,7 +63,7 @@ func (c *CodecEntity) EncodePacket(packets []*packet.Packet) ([]byte, error) {
 	copyData := make([]byte, len(data))
 	copy(copyData, data)
 
-	return data, nil
+	return copyData, nil
 }
 
 func (c *CodecEntity) DecodePacket(data []byte) ([]*packet.Packet, error) {
