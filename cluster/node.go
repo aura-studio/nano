f83833ce59a004c81c0c1494ace60b84a861f5a9
lizongti <lzongti@gmail.com>
2023-09-20 17:47:16 +0800

    fix http handler

--- cluster/node.go
+++ cluster/node.go
@@ -389,7 +389,10 @@ func (n *Node) listenAndServeTCP() {
 
 func (n *Node) listenAndServeHttp() {
 	router := mux.NewRouter()
-	router.HandleFunc("/{instance:[0-9]*}/*", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/{instance:[0-9]*}", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	router.PathPrefix("/{instance:[0-9]*}/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, "/"+mux.Vars(r)["instance"])
 		router.ServeHTTP(w, r)
 	})
