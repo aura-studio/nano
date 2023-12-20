38d7908fd2b5e962127538bfd69f376747d24fe6
lizongti <lzongti@gmail.com>
2023-12-21 00:13:48 +0800

    fix websocket upgrader

--- upgrader/wsupgrader/upgrader.go
+++ upgrader/wsupgrader/upgrader.go
@@ -24,6 +24,8 @@ func NewUpgrader() *Upgrader {
 }
 
 func (u *Upgrader) Upgrade(w http.ResponseWriter, r *http.Request, params map[string]string) (net.Conn, error) {
+	r.Header.Set("Connection", "Upgrade")
+	r.Header.Set("Upgrade", "websocket")
 	conn, err := u.Upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return nil, err
